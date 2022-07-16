b3ccafc70716a00103d496f08774d6c5a80b1a75
Eric Soon <eric.soonsmk@gmail.com>
2022-07-16 10:32:25 -0700

    added the hero banner

---
+++ src/components/HeroBanner/style.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles(() => ({
+  heroBannerImg: {
+    position: 'absolute',
+    right: ' 40px',
+    top: '0px',
+    width: '700px',
+    height: '900px',
+    marginTop: '-330px',
+
+  },
+}));
