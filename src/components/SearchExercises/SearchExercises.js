dd6f0b913fadf0ca6a19049055dc5fa673499e2f
Eric Soon <eric.soonsmk@gmail.com>
2022-07-16 11:14:58 -0700

    added search component

--- src/components/SearchExercises/SearchExercises.js
+++ src/components/SearchExercises/SearchExercises.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
+import useStyles from './styles';
 
 function SearchExercises() {
-  console.log('searchexercises');
+  const classes = useStyles();
+  const [search, setSearch] = useState('');
+
+  const handleSearch = async () => {
+    if (search) {
+      // const exercisesData = await fetchData();
+    }
+  };
+
   return (
-    <div>SearchExercises</div>
+    <Stack
+      alignItems="center"
+      mt="37px"
+      justifyContent="center"
+      p="20px"
+    >
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: '44px', xs: '30px' } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises <br /> You Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+            width: { lg: '800px', xs: '350px' },
+            backgroundColor: '#fff',
+            borderRadius: '40px',
+          }}
+          height="76px"
+          value=""
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button
+          className={classes.searchBtn}
+          sx={{
+            bgcolor: '#FF2625',
+            color: '3FFF',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px' },
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
+            position: 'absolute',
+            right: '0',
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
   );
 }
 
---
+++ src/components/SearchExercises/styles.js
@@ -0,0 +1,8 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles(() => ({
+  searchBtn: {
+    color: '#FF2625 !important',
+    border: '1px solid #FF2625 !important',
+  },
+}));
