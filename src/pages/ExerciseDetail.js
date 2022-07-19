d51026857b8e4fb0ba63d423b38dad297bb0e003
Eric Soon <eric.soonsmk@gmail.com>
2022-07-18 17:27:45 -0700

    added exercise detail, videos, and detail components

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import Detail from '../components/Detail/Detail';
+import ExerciseVideos from '../components/ExerciseVideos/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises/SimilarExercises';
 
 function ExerciseDetail() {
-  console.log('exercisedetails');
+  const [exerciseDetail, setexerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com/video/related';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercoses/${id}`, exerciseOptions);
+      setexerciseDetail(exerciseDetail);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   );
 }
 
